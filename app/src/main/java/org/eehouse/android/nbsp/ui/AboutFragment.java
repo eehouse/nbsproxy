cba1240292ba0fe6b2286203323355535e6f6c9a
Eric House <eehouse@eehouse.org>
2019-03-30 09:36:32 -0700

    add build and copyright info; unique apk name
    Bring over code to give the .apk a name based on git rev and to include
    build time and other info in About fragment.

--- app/src/main/java/org/eehouse/android/nbsp/ui/AboutFragment.java
+++ app/src/main/java/org/eehouse/android/nbsp/ui/AboutFragment.java
@@ -19,5 +19,38 @@
 
 package org.eehouse.android.nbsp.ui;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.eehouse.android.nbsp.R;
+import org.eehouse.android.nbsp.BuildConfig;
+
 public class AboutFragment extends PageFragment {
+    private static final String TAG = AboutFragment.class.getSimpleName();
+
+    @Override
+    public View onCreateView( LayoutInflater inflater,
+                              ViewGroup container,
+                              Bundle savedInstanceState )
+    {
+        View view = super.onCreateView( inflater, container, savedInstanceState );
+
+        TextView tv = (TextView)view.findViewById(R.id.version_text);
+        DateFormat df = DateFormat.getDateTimeInstance( DateFormat.FULL,
+                                                        DateFormat.FULL );
+        String dateString = df.format( new Date( BuildConfig.BUILD_STAMP_SECS * 1000 ) );
+        tv.setText( getString( R.string.version_text_fmt,
+                               BuildConfig.VERSION_NAME,
+                               BuildConfig.GIT_REV,
+                               dateString ) );
+
+        return view;
+    }
 }
